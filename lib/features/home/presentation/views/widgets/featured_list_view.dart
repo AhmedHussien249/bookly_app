533b0acff8a50dfe54b5c8cb6b875dc51c33f96c
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2025-07-01 16:35:33 +0300

    featured list view & featured list view item done

---
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'custom_list_view_item.dart';
+
+class FeaturedListView extends StatelessWidget {
+  const FeaturedListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => const Padding(
+          padding: EdgeInsets.only(left: 16),
+          child: FeaturedListViewItem(),
+        ),
+      ),
+    );
+  }
+}
