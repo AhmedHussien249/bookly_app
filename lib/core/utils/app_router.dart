f6c14d2845ea615bb633500ad9a5cb5e467c0688
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2025-07-03 04:10:39 +0300

    search view ui done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,6 +9,7 @@ import '../../features/splash/presentation/views/splash_view.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -37,6 +39,23 @@ abstract class AppRouter {
           return CustomTransitionPage(
             key: state.pageKey,
             child: const BookDetailsView(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            },
+            transitionDuration: const Duration(milliseconds: 500),
+          );
+        },
+      ),
+      GoRoute(
+        path: kSearchView,
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const SearchView(),
             transitionsBuilder:
                 (context, animation, secondaryAnimation, child) {
               return FadeTransition(
---
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view.dart';
+import 'custom_Search_text_field.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomSearchTextField(),
+                SizedBox(height: 16),
+                Text(
+                  'Search Results',
+                  style: Styles.textStyle18,
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+        BestSellerSliverListView(),
+      ],
+    );
+  }
+}
