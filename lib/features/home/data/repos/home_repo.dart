c524c649d0f761fa0881d621b2c9e0f5aef76c27
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2025-07-05 05:13:15 +0300

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,7 @@
+abstract class HomeRepo {
+  fetchBestSellerBooks();
+  fetchFeaturedBooks();
+}
+/* leh bnst5dm repository pattern 
+   3shan y3rfk el featured d ht3ml a w el methods el hnst5dmha
+*/
